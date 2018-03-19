7809d9a3d90bf52670328e8b0cd32eb960dc69ea
Nacho Mascort <nacho.mascort@gmail.com>
2018-03-19 16:27:03 +0100

    Update and rename urls_sitemap_index.py to extract_urls_sitemap.py

---
+++ extract_urls_sitemap.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def extract_urls_from_sitemap(url_sitemap):
+    sitemap = []
+    urls = []
+    xml = requests.get(url_sitemap)
+    xml_parsed = BeautifulSoup(xml.content, "xml")
+    if xml_parsed.find_all("sitemap"):
+        sitemaps = xml_parsed.find_all("loc")
+        for s in sitemaps:
+            sitemap.append(s.text)
+        for s in sitemap:
+            uis = requests.get(s)
+            uis_parsed = BeautifulSoup(uis.content, "xml")
+            urls_loc1 = uis_parsed.find_all("loc")
+            for u in urls_loc1:
+                urls.append(u.text)
+    else:
+        url = xml_parsed.find_all("loc")
+        for u in url:
+            urls.append(u.text)
+    return urls
+
+urls = extract_urls_from_sitemap("https://www.ohlibro.com/sitemap/books/1") # poner la URL dentro de las comillas
+df = pd.DataFrame({"urls":urls})
+df.to_csv("urls-sitemap.csv", sep="\t", encoding="utf-8")

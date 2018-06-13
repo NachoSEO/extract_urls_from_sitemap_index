6efc0369281d91f56c958eff43599129bdef470a
Nacho Mascort <nacho.mascort@gmail.com>
2018-06-13 13:21:33 +0200

    Identify in which sitemap is the URL

--- extract_urls_sitemap.py
+++ extract_urls_sitemap.py
@@ -5,6 +5,7 @@ import pandas as pd
 def extract_urls_from_sitemap(url_sitemap):
     sitemap = []
     urls = []
+    sitemap_crawled = []
     xml = requests.get(url_sitemap)
     xml_parsed = BeautifulSoup(xml.content, "xml")
     if xml_parsed.find_all("sitemap"):
@@ -17,12 +18,18 @@ def extract_urls_from_sitemap(url_sitemap):
             urls_loc1 = uis_parsed.find_all("loc")
             for u in urls_loc1:
                 urls.append(u.text)
+                sitemap_crawled.append(s)
     else:
         url = xml_parsed.find_all("loc")
         for u in url:
             urls.append(u.text)
-    return urls
+            sitemap_crawled.append(url_sitemap)
+    return urls, sitemap_crawled
 
-urls = extract_urls_from_sitemap("URL HERE")
-df = pd.DataFrame({"urls":urls})
+urls,sitemap_crawled = extract_urls_from_sitemap("URL-HERE")
+
+df = pd.DataFrame({
+    "urls":urls,
+    "sitemap":sitemap_crawled
+})
 df.to_csv("urls-sitemap.csv", sep="\t", encoding="utf-8")
